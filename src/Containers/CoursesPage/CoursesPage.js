ce1a806babef64caa3711879b2e7f56ff5ac836e
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-02-08 16:39:39 +0200

    finished stage

--- src/Containers/CoursesPage/CoursesPage.js
+++ src/Containers/CoursesPage/CoursesPage.js
@@ -1,56 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import CourseListItem from '../../Components/CourseListItem/CourseListItem'
+import CourseListItem from "../../Components/CourseListItem/CourseListItem";
+import CourseOverView from "../../Components/CourseOverview/CourseOverview";
 
-import classes from './CoursesPage.module.css'
+import classes from "./CoursesPage.module.css";
 
 class CoursesPags extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          loadedCourses: {
-            1: {
-              CourseName: "Distributed Systems",
-              DoctorName: "Dr. Gamal Abd El-Shafy",
-            },
-            2: {
-              CourseName: "Physics 2",
-              DoctorName: "Dr. Wael Fekry",
-            },
-            3: {
-              CourseName: "Math 3",
-              DoctorName: "Dr. Makram",
-            },
-          },
-          displayedCourse: null,
-        };
-      }
-    
-      selectingCourseHandler = (courseid) => {
-          alert(courseid)
-        this.setState({displayedCourse: courseid});
-      }
-    
-      render() {
-        let loadedCourses = Object.keys(this.state.loadedCourses).map((key) => {
-          return (
-              <CourseListItem
-                {...this.state.loadedCourses[key]}
-                id={key}
-                getSelected = {this.selectingCourseHandler}
-                height = {120}
-              />        
-          );
-        });
-        return <div className={classes.CoursesPage} >
-            <div className={classes.CoursesList} >
-            {loadedCourses}
-            </div>
-            <div className = {classes.CourseOverview} >
-              {/* {Todo: add the course overview part} */}
-            </div>
-        </div>;
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadedCourses: {
+        1: {
+          CourseName: "Distributed Systems",
+          DoctorName: "Dr. Gamal Abd El-Shafy",
+        },
+        2: {
+          CourseName: "Physics 2",
+          DoctorName: "Dr. Wael Fekry",
+        },
+        3: {
+          CourseName: "Math 3",
+          DoctorName: "Dr. Makram",
+        },
+      },
+      displayedCourse: null,
+    };
+  }
+
+  selectingCourseHandler = (courseid) => {
+    this.setState({ displayedCourse: courseid });
+  };
+
+  render() {
+    let loadedCourses = Object.keys(this.state.loadedCourses).map((key) => {
+      return (
+        <CourseListItem
+          {...this.state.loadedCourses[key]}
+          id={key}
+          getSelected={this.selectingCourseHandler}
+          height={120}
+        />
+      );
+    });
+
+    let stage = (
+      <div className={classes.CourseOverview}>
+        <CourseOverView
+          {...this.state.loadedCourses[this.state.displayedCourse]}
+        />
+      </div>
+    );
+    if (this.state.displayedCourse === null) {
+      stage = null;
+    }
+
+    return (
+      <div className={classes.CoursesPage}>
+        <div className={classes.CoursesList}>{loadedCourses}</div>
+        {stage}
+      </div>
+    );
+  }
 }
- 
+
 export default CoursesPags;
