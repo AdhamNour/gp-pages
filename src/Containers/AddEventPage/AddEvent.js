5c950c1993215259a33db0a3696babfd276b8db9
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-03-19 21:59:45 +0200

    finished AddEvent Page

--- src/Containers/AddEventPage/AddEvent.js
+++ src/Containers/AddEventPage/AddEvent.js
@@ -1,13 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import classes from "./AddEvent.module.css";
 import InputPart from "./LeftPart/InputPart";
-
+import Notification from "../../Components/AddEvent/Notifications/Notifications";
+import {Button} from "@material-ui/core"
 class AddEvent extends Component {
   render() {
     return (
-      <div className={classes.AddEvent}>
-       <InputPart/>
-      </div>
+      <Fragment>
+        <div className={classes.AddEvent}>
+          <InputPart />
+          <div>
+            <Notification />
+          </div>
+        </div>
+        <div className={classes.CreateDiscardContainer} >
+        <Button color="secondary">
+            Cancel
+          </Button>
+          <Button variant="contained" color="primary" >
+            Create
+          </Button>
+          
+        </div>
+      </Fragment>
     );
   }
 }
