a5db831463f300b950f9093386e17473ded0dc26
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-03-21 13:20:33 +0200

    finished addEvent Modal

--- src/Containers/AddEventPage/AddEvent.js
+++ src/Containers/AddEventPage/AddEvent.js
@@ -2,50 +2,59 @@ import React, { Component, Fragment } from "react";
 import classes from "./AddEvent.module.css";
 import InputPart from "./InputPart/InputPart";
 import NotificationPart from "./NotificationsPart/NotificationsPart";
-import { Button,Modal,Paper } from "@material-ui/core";
+import { Button, Modal, Paper } from "@material-ui/core";
 class AddEvent extends Component {
   state = {
-    openedModal: false
-  }
+    openedModal: false,
+  };
   openModal = () => {
-    console.log("Hello from OpenModal")
+    console.log("Hello from OpenModal");
     this.setState({
-      openedModal: true
-    })
-  }
+      openedModal: true,
+    });
+  };
   closeModal = () => {
     this.setState({
-      openedModal: false
-    })
-  }
-   body =  (
+      openedModal: false,
+    });
+  };
+  body = (
     <Paper className={classes.ModalContainer}>
       <div className={classes.AddEvent}>
         <InputPart />
         <NotificationPart />
       </div>
       <div className={classes.CreateDiscardContainer}>
-        <Button color="secondary">Cancel</Button>
-        <Button variant="contained" color="primary">
+        <Button color="secondary" onClick={this.closeModal}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            alert("The Event is being created");
+            this.closeModal();
+          }}
+        >
           Create
         </Button>
       </div>
     </Paper>
-  )
+  );
   render() {
-    return(
+    return (
       <Fragment>
-        <Button color="primary" variant="contained" onClick ={this.openModal} >
+        <Button color="primary" variant="contained" onClick={this.openModal}>
           Add Event
         </Button>
         <Modal
-        open={this.state.openedModal}
-        onClose={this.closeModal}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {this.body}
-      </Modal>
+          open={this.state.openedModal}
+          onClose={this.closeModal}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          {this.body}
+        </Modal>
       </Fragment>
     );
   }
--- src/Containers/AddEventPage/InputPart/InputPart.js
+++ src/Containers/AddEventPage/InputPart/InputPart.js
@@ -1,67 +1,27 @@
-import React from 'react';
+import React from "react";
 import { TextField, Grid } from "@material-ui/core";
 import { Today, Event, Schedule } from "@material-ui/icons";
-import classes from "./InputPart.module.css"
-
-//TODO: add some padding and margin to the input fields down below 
+import classes from "./InputPart.module.css";
+import Input from '../../../Components/Input/Input'
+//TODO: add some padding and margin to the input fields down below
 
 const InputPart = (props) => {
-    return ( 
-        <div>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <Event />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="With a grid" />
-          </Grid>
-        </Grid>
-        <div className={classes.row}>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <Today />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="With a grid" />
-            </Grid>
-          </Grid>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <Event />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="With a grid" />
-            </Grid>
-          </Grid>
-        </div>
-        <div className={classes.row}>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <Schedule />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="With a grid" />
-            </Grid>
-          </Grid>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <Schedule />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="With a grid" />
-            </Grid>
-          </Grid>
-        </div>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <Event />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="With a grid" />
-          </Grid>
-        </Grid>
+  return (
+    <div  >
+      <Input icon ={<Event/>} />
+      <div className={classes.row}>
+        <Input icon ={<Today/>} />
+        <Input icon ={<Event/>} />
+
       </div>
-     );
-}
- 
+      <div className={classes.row}>
+        <Input icon ={<Schedule/>} />
+        <Input icon ={<Schedule/>} />
+
+      </div>
+      <Input icon ={<Event/>} />
+    </div>
+  );
+};
+
 export default InputPart;
