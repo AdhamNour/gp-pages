09d0b70ca9c4dae34ec429ef16d8b4fec1a2cad9
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-03-22 00:33:29 +0200

    trying to finish the strage tringle

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   
   render() {
     
-    return <AddEvent />;
+    return <CoursesPage />;
   }
 }
 
--- src/Components/CoursesPageComponents/CourseListItem/CourseListItem.js
+++ src/Components/CoursesPageComponents/CourseListItem/CourseListItem.js
@@ -2,27 +2,29 @@ import React, { Component } from "react";
 import classes from "./CourseListItem.module.css";
 import CourseItemContent from "./CourseItemContent/CourseItemContent";
 class CourseListItem extends Component {
-
   render() {
-    const testImage =this.props.CoursePicture;
+    const testImage = this.props.CoursePicture;
     let selected = this.props.displayedCourse === this.props.id;
-    console.log("From CourseItem.js displayedCourse ID = "+this.props.displayedCourse)
-    console.log("From CourseItem.js  ID = "+this.props.id)
-    console.log("From CourseItem.js: selected is"+selected)
     return (
-      <div
-        className={[classes.ItemContainer,selected?classes.selected:""].join(" ")}
-        onClick={() => {
-          this.props.getSelected(this.props.id);
-        }}
-      >
-        <img src={testImage} alt="tst" className={classes.CourseImage} />
-        <div className={classes.CourseItemContent}>
-          <CourseItemContent
-            CourseName={this.props.CourseName}
-            DoctorName={this.props.DoctorName}
-          />
+      <div className={classes.CourseItemListContainer} >
+        <div
+          className={[
+            classes.ItemContainer,
+            selected ? classes.selected : "",
+          ].join(" ")}
+          onClick={() => {
+            this.props.getSelected(this.props.id);
+          }}
+        >
+          <img src={testImage} alt="tst" className={classes.CourseImage} />
+          <div className={classes.CourseItemContent}>
+            <CourseItemContent
+              CourseName={this.props.CourseName}
+              DoctorName={this.props.DoctorName}
+            />
+          </div>
         </div>
+        {selected?<div className={classes.Tringle}></div>:null}
       </div>
     );
   }
